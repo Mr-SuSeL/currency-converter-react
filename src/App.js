9da74ff4951e6fc8ca95fec4d1e743a86f2702f6
Łukasz Susłowicz <susel.invest@gmail.com>
2023-09-13 18:40:23 +0100

    timer functionality fullfilled

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import Container from "./Container";
 import Form from "./Form";
 import { currencies } from "./currencies";
+import Timer from "./Timer";
 
 function App() {
-  const [result, setResult] = useState();
+  const [result, setResult] = useState(undefined);
 
   const calcResult = (currency, number) => {
     const rate = currencies.find(({ code }) => code === currency).rate;
@@ -19,6 +20,7 @@ function App() {
 
   return (
     <Container>
+      <Timer />
       <h1>Currency Converter in React</h1>
       <Form result={result} calcResult={calcResult} />
     </Container>
---
+++ src/Timer/index.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./style.css";
+
+const Timer = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <p className="timer__date">
+      Dzisiaj jest:{" "}
+      {date.toLocaleString("pl-PL", {
+        weekday: "long",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        day: "numeric",
+        month: "long",
+      })}
+    </p>
+  );
+};
+
+export default Timer;
