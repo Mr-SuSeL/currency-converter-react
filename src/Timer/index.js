8038585d1754f546ab5ecbab2cec84527370bef7
Łukasz Susłowicz <susel.invest@gmail.com>
2023-09-15 18:20:00 +0100

    useCurrentDate hook accomplished

--- src/Timer/index.js
+++ src/Timer/index.js
@@ -1,31 +1,25 @@
-import { useState, useEffect } from "react";
 import "./style.css";
+import { useCurrentDate } from "./useCurrentDate";
+
+const actualDate = (date) => 
+  date.toLocaleString("pl-PL", {
+  weekday: "long",
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+  day: "numeric",
+  month: "long",
+  timeZone: "UTC",
+});
+
 
 const Timer = () => {
-  const [date, setDate] = useState(new Date(Date));
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+  const date = useCurrentDate();
 
   return (
     <p className="timer__date">
       Dzisiaj jest:{" "}
-      {date.toLocaleString("pl-PL", {
-        weekday: "long",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        day: "numeric",
-        month: "long",
-        timeZone: "UTC",
-      })}
+      {actualDate(date)}
     </p>
   );
 };
